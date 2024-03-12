4111bb9c1fed6772547df303d257675f74f98654
Vishal31122000 <singh.vish3112000@gmail.com>
2024-03-12 23:29:28 +0530

    update the eks cluster

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -1,4 +1,4 @@
-module "  " {
+module "eks_cluster" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.19.1"
 
@@ -11,25 +11,19 @@ module "  " {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
     one = {
       name = "node-group-1"
-
       instance_types = ["t3.small"]
-
       min_size     = 1
       max_size     = 2
       desired_size = 2
     }
-
     two = {
       name = "node-group-2"
-
       instance_types = ["t3.small"]
-
       min_size     = 1
       max_size     = 2
       desired_size = 1
