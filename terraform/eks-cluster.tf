d99032497856f078042927ab3f28654d7cf44a63
Vishal31122000 <singh.vish3112000@gmail.com>
2024-03-12 23:58:53 +0530

    made changes

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -5,7 +5,7 @@ module "eks_cluster" {
   cluster_name    = var.clusterName
   cluster_version = "1.27"
 
-  vpc_id                         = 
+  vpc_id                         = module.vpc.vpc_id
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
