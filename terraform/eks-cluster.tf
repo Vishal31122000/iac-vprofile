7642a1b0fe50f6624f237133f997fd0101fb1f6a
Vishal31122000 <singh.vish3112000@gmail.com>
2024-03-12 22:16:44 +0530

    added bucket name

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -21,7 +21,7 @@ module "  " {
       instance_types = ["t3.small"]
 
       min_size     = 1
-      max_size     = 3
+      max_size     = 2
       desired_size = 2
     }
 
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -27,7 +27,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "gitops-project"
+    bucket = "buc-gitops"
     key    = "terraform.tfstate"
     region = "us-east-1"
   }
